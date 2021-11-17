19d0c39942fc43de559e7aecb5b98cb976bb8708
Rhino <hungbq.study@gmail.com>
2021-11-18 02:22:40 +0700

    'update'

--- WebAdvert.SearchWorker/ElasticSearchHelper.cs
+++ WebAdvert.SearchWorker/ElasticSearchHelper.cs
@@ -3,18 +3,20 @@ using Nest;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebAdvert.SearchWorker.Models;
 
 namespace WebAdvert.SearchWorker
 {
     public class ElasticSearchHelper
     {
         private static IElasticClient _client;
-        public static IElasticClient GetInstance(IConfiguration config )
+        public static IElasticClient GetInstance(IConfiguration config)
         {
             if (_client == null)
             {
                 var url = config.GetSection("ES").GetValue<string>("url");
-                var settings = new ConnectionSettings(new Uri(url)).DefaultIndex("adverts");
+                var settings = new ConnectionSettings(new Uri(url)).DefaultIndex("adverts")
+                                                                   .DefaultMappingFor<AdvertType>(m => m.IdProperty(x => x.Id));
                 _client = new ElasticClient(settings);
             }
 
